a74f5fdfe005916773ead3ea925153156f709a10
MarcinJaszczynski <m.jaszczynski@gmail.com>
2025-03-10 14:47:48 +0100

    zmiany model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
     // ...
  
-    public function canAccessFilament(): bool
+    public function canAccessFilament(Panel $panel): bool
     {
         return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
